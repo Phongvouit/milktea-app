1ff03c7a399e3e70f7ce840fe254be472c494837
Phongvo123 <19522015@gm.uit.edu.vn>
2024-09-14 11:33:50 +0700

    Single Product Page Design

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Price from "@/components/Price";
+import Image from "next/image";
 
 const SingleProductPage = () => {
   return (
-    <div>SingleProductPage</div>
-  )
-}
+    <div className="w-full">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full rounded border border-gray-200">
+            <div className="w-2/3 mx-auto my-4 relative pt-[80%] md:pt-[100%]">
+              <Image
+                alt="ecommerce"
+                className="object-cover"
+                src="/images/product.png"
+                fill
+              />
+            </div>
+          </div>
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <h1 className="text-green-800 text-3xl title-font font-medium mb-6">
+              Trà sữa Matcha
+            </h1>
+            <p className="leading-relaxed font-normal text-base">
+              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
+              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
+              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
+              seitan poutine tumeric. Gastropub blue bottle austin listicle
+              pour-over, neutra jean shorts keytar banjo tattooed umami
+              cardigan.
+            </p>
+            <Price />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SingleProductPage
+export default SingleProductPage;
--- src/components/ItemProduct.tsx
+++ src/components/ItemProduct.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ItemProduct = () => {
   return (
     <div className="rounded-md bg-white px-[15px] py-[30px] hover:shadow-lg hover:border-[2px]">
-      <div className="w-2/3 mx-auto relative mb-3 pt-[80%] md:pt-[100%]">
+      <div className="w-2/3 mx-auto relative mb-3 pt-[80%] md:h-48 md:pt-0">
         <Image
           className="object-cover"
           src="/images/product.png"
@@ -19,9 +20,11 @@ const ItemProduct = () => {
           Matcha Milk Tea
         </p>
         <p className="mb-[15px] text-lg font-black text-green-800">45.000 đ</p>
-        <button className="rounded-md border border-green-800 px-4 py-1 text-green-800 hover:bg-green-800 hover:text-white">
-          Đặt hàng
-        </button>
+        <Link href="/product/1">
+          <button className="rounded-md border border-green-800 px-4 py-1 text-green-800 hover:bg-green-800 hover:text-white">
+            Đặt hàng
+          </button>
+        </Link>
       </div>
     </div>
   );
